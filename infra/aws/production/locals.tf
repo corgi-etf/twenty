2b3d449cdb135eba63ebeea7888c26557dcd488e
damienwiese <damienwiese@gmail.com>
2026-09-06 17:25:19 -0500

    fix(infra): declare US data hosting region

--- infra/aws/production/locals.tf
+++ infra/aws/production/locals.tf
@@ -23,6 +23,7 @@ locals {
     { name = "NODE_ENV", value = "production" },
     { name = "NODE_PORT", value = "3000" },
     { name = "SERVER_URL", value = "https://${var.domain_name}" },
+    { name = "DPA_DEPLOYMENT_REGION", value = "US" },
     { name = "REDIS_URL", value = "rediss://${aws_elasticache_replication_group.crm.primary_endpoint_address}:6379" },
     { name = "STORAGE_TYPE", value = "S_3" },
     { name = "STORAGE_S3_REGION", value = var.aws_region },
